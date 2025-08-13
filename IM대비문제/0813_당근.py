15684f407f65ef660c64f36789e8a1bf7a662cba
DESKTOP-BTDJ0S7\Administrator <enwls5626@gmail.com>
2025-08-13 22:50:17 +0900

    0813_read

--- IM대비문제/0813_당근.py
+++ IM대비문제/0813_당근.py
@@ -1,30 +1,30 @@
-T = int(input())
-for test_case in range(1,T+1):
-    n = int(input())
-    arr = list(map(int,input().split()))
-    count = 1
-
-    for i in range(n-1):
-        if arr[i] < arr[i+1]:
-            count += 1
-        else:
-            count = 1
-        
-    print("이거다",count)
-# -------------------------------------------------------
 # T = int(input())
-# for test_case in range(1, T+1):
+# for test_case in range(1,T+1):
 #     n = int(input())
-#     arr = list(map(int, input().split()))
-    
-#     max_len = 1
-#     cur_len = 1
+#     arr = list(map(int,input().split()))
+#     count = 1
 
 #     for i in range(n-1):
 #         if arr[i] < arr[i+1]:
-#             cur_len += 1
-#             max_len = max(max_len, cur_len)
+#             count += 1
 #         else:
-#             cur_len = 1
+#             count = 1
+        
+#     print("이거다",count)
+# -------------------------------------------------------
+T = int(input())
+for test_case in range(1, T+1):
+    n = int(input())
+    arr = list(map(int, input().split()))
     
-#     print(f"#{test_case} {max_len}")
+    max_len = 1
+    cur_len = 1
+
+    for i in range(n-1):
+        if arr[i] < arr[i+1]:
+            cur_len += 1
+            max_len = max(max_len, cur_len)
+        else:
+            cur_len = 1
+    
+    print(f"#{test_case} {max_len}")
