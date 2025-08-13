089fdae58ec4a1d457e8c8afdacaf8cb1a2fb9e5
dujin <enwls5626@gmail.com>
2025-08-13 09:04:10 +0900

    유신형님도움

--- im대비보충/0812_IM복기.py
+++ im대비보충/0812_IM복기.py
@@ -23,7 +23,33 @@ for test_case in range(1,T+1):
                 if 0 <= ni < N and 0 <= nj < N:
                     if arr[ni][nj] < arr[start_r][start_c]:
                         current = arr[ni][nj]
-                        arr[i][j] = arr[ni][nj]
                         total_count += 1
+            break
+    print(f'#{test_case} {total_count}')
+
+
+
+# ------------------------------------------------------------
+T = int(input())
+dxy = [[1,0],[-1,0],[0,1],[0,-1]]
+for test_case in range(1,T+1):
+    N= int(input())
+    start_r, start_c = map(int,input().split())
+    arr = [list(map(int,input().split())) for _ in range(N)]
+    total_count = 1
+
+    x, y = start_r, start_c
+    while True:
+        current_num = arr[x][y]
+        for dx, dy in dxy:
+            nx = x + dx
+            ny = y + dy
+            if 0 <= nx < N and 0 <= ny < N:
+                next_num = arr[nx][ny]
+                if next_num < current_num:
+                    x, y = nx, ny
+                    total_count += 1
+                    break
+        else:
             break
     print(f'#{test_case} {total_count}')
