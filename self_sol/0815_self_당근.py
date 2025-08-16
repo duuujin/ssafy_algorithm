109139295b528cc4dba4fcb0bebe51f4de6651cf
DESKTOP-BTDJ0S7\Administrator <enwls5626@gmail.com>
2025-08-16 22:55:34 +0900

    carrot

--- self_sol/0815_self_당근.py
+++ self_sol/0815_self_당근.py
@@ -17,14 +17,13 @@ for test_case in range(1, T+1):
     n = int(input())
     arr = list(map(int, input().split()))
     
-    max_len = 1
+    max_len  = 1
     cur_len = 1
 
     for i in range(n-1):
         if arr[i] < arr[i+1]:
             cur_len += 1
-            max_len = max(max_len, cur_len)
+            max_len = max(cur_len,max_len)
         else:
             cur_len = 1
-    
-    print(f"#{test_case} {max_len}")
+    print(f'#{test_case} {max_len}')
