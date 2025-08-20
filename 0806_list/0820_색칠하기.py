5a09c7e88cb6ab1e94fd7423f865c24ce00c95dd
duuujin <enwls5626@gmail.com>
2025-08-20 16:03:50 +0900

    0820_gravity_해결

--- 0806_list/0820_색칠하기.py
+++ 0806_list/0820_색칠하기.py
@@ -2,3 +2,15 @@ T = int(input())
 for test_case in range(1,T+1):
     n = int(input())
     arr = list(map(int,input().split()))
+    max_cnt = 0
+
+    for i in range(n):
+        nak = 0
+        for j in range(i+1,n):
+            if arr[i] <= arr[j] :
+                nak += 0
+            elif arr[i] > arr[j]:
+                nak += 1
+        max_cnt = max(max_cnt, nak)
+    
+    print(f'#{test_case} {max_cnt}')
