44d9cbd6b4f25af6e6622cfc3d2518f70331bbf2
duuujin <enwls5626@gmail.com>
2025-08-21 15:58:20 +0900

    0821_숫자배열_진행중

--- 0806_list/0820_gravity.py
+++ 0806_list/0820_gravity.py
@@ -8,7 +8,7 @@ for test_case in range(1,T+1):
         nak = 0
         for j in range(i+1,n):
             if arr[i] <= arr[j] :
-                nak += 0
+                nak += 0 
             elif arr[i] > arr[j]:
                 nak += 1
         max_cnt = max(max_cnt, nak)
