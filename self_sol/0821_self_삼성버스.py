b99677373877ad173f0ce31f0233f3d6caec6b2c
duuujin <enwls5626@gmail.com>
2025-08-21 19:48:16 +0900

    0821_삼성버스

--- self_sol/0821_self_삼성버스.py
+++ self_sol/0821_self_삼성버스.py
@@ -3,5 +3,10 @@ for test_case in range(1,T+1):
     n = int(input())
     a1, b1 = map(int,input().split())
     a2, b2 = map(int,input().split())
+    p = int(input())
+    c1 = int(input())
+    c2 = int(input())
+    c3 = int(input())
+    c4 = int(input())
+    c5 = int(input())
     
-
