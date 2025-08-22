75fd627e53d255dffcbfe607fcfb19d75d92083a
duuujin <enwls5626@gmail.com>
2025-08-22 09:02:54 +0900

    continue

--- self_sol/0821_self_삼성버스.py
+++ self_sol/0821_self_삼성버스.py
@@ -1,14 +1,23 @@
 T = int(input())
 for test_case in range(1,T+1):
-    n = int(input())
-    a1, b1 = map(int,input().split())
-    a2, b2 = map(int,input().split())
-    p = int(input())
-    c1 = int(input())
-    c2 = int(input())
-    c3 = int(input())
-    c4 = int(input())
-    c5 = int(input())
-    result = [0]*p
+    # result = [0]*5001
+
+    # n = int(input())
+    # for nosun in range(n):
+    #     a1,b1 = map(int,input().split())
+    #     for j in range(a1,b1+1):
+    #         result[j] += 1
     
+    # p = int(input())
+    # bus_list = []
+    # for i in range(p):
+    #     a = int(input())
+    #     bus_list.append(a)
+
+    # print('#{}'.format(test_case), end=' ')
+    # for i in bus_list:
+    #     print(result[i], end=' ')
+    # print()
+    # ------------------------------------------------------------
+    n = int(input())
     
