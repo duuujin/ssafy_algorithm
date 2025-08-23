44528901b204e4498578db5fa3ad0d7b1de6b002
duuujin <enwls5626@gmail.com>
2025-08-23 15:49:49 +0900

    0823_ladder_clear

--- self_sol/0822_ladder1.py
+++ self_sol/0822_ladder1.py
@@ -1,3 +1,22 @@
-# T = int(input())
-# for test_case in range(1,T+1):
-    
+import sys
+sys.stdin = open("/Users/chaeng/Desktop/duujin/ssafy_algorithm/self_sol/input (4).txt","r")
+for test_case in range(1,11):
+    T = int(input())
+    arr = [list(map(int,input().split())) for _ in range(100)]
+    visited = [[False] * 100 for _ in range(100)]
+    dxy = [[0,1],[0,-1],[-1,0]]
+    for i in range(100):
+        if arr[99][i] == 2:
+            x,y = (99,i)
+            break
+        
+    visited[x][y] = True
+    while x:
+        for dx,dy in dxy:
+            nx = x+dx
+            ny = y+dy
+            if 0 <= nx < 100 and 0 <= ny < 100 and not visited[nx][ny] and arr[nx][ny] == 1:
+                visited[nx][ny] = True
+                x,y = nx,ny
+                break
+    print(f'#{test_case} {y}')
