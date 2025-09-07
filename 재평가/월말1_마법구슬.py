cee3c58f7973d2af6a31fe0cd4e35df22d7321ea
duuujin <enwls5626@gmail.com>
2025-09-07 15:50:23 +0900

    알고리즘재평가대비

--- 재평가/월말1_마법구슬.py
+++ 재평가/월말1_마법구슬.py
@@ -2,5 +2,5 @@ from itertools import combinations, permutations, product, combinations_with_rep
 n = 10
 k1 = 8
 k2 = 10
-
-# 서로 다른 n개 중에서 k1개를 뽑는 조합
+for idx in combinations(range(1,n+1), 2):
+    print(list(idx))
---
+++ 재평가/파리추천.py
@@ -0,0 +1,27 @@
+T = int(input())
+for test_case in range(1, T+1):
+    n , m = map(int,input().split())
+    arr = [list(map(int,input().split())) for _ in range(n)]
+    dxy = [[0,1],[0,-1],[1,0],[-1,0],[1,1],[1,-1],[-1,1],[-1,-1]]
+    total_sum = 0
+
+    for i in range(n):
+        for j in range(n):
+            plus_sum = arr[i][j]
+            for dx,dy in dxy[:4]:
+                for step in range(1,m):
+                    ni = i + dx*step
+                    nj = j + dy*step
+                    if 0 <= ni < n and 0 <= nj < n:
+                        plus_sum += arr[ni][nj]
+            
+            x_sum = arr[i][j]
+            for dx,dy in dxy[4:]:
+                for step in range(1,m):
+                    ni = i +dx*step
+                    nj = j + dy*step
+                    if 0 <= ni < n and 0 <= nj < n:
+                        x_sum += arr[ni][nj]
+            total_sum = max(total_sum,plus_sum,x_sum)
+    print("--------------------------")
+    print(total_sum)
