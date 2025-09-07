5975db94aefb7836a8df7b6d60ddd7634c6d3c15
duuujin <enwls5626@gmail.com>
2025-09-07 14:38:52 +0900

    월말평가풀어보기

---
+++ 재평가/조합.py
@@ -0,0 +1,19 @@
+T = int(input())
+for test_case in range(1, T+1):
+    n = int(input())
+    arr = [list(map(int,input().split())) for _ in range(n)]
+    dxy = [[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2]]
+    start = 0
+    total_max = 0
+    
+    for i in range(n):
+        for j in range(n):
+            start = arr[i][j]
+            for dx,dy in dxy:
+                ni = (i) + dx
+                nj = (j) + dy
+                if 0 <= ni < n and 0 <= nj < n:
+                    start += arr[ni][nj]
+                    total_max = max(total_max,start)
+    print(total_max)
+                
