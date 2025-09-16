6ac08402c4841a641796388e273bc892f99bf687
duuujin <enwls5626@gmail.com>
2025-09-16 16:34:17 +0900

    subtree문제풀이

--- 0915_트리/subtree.py
+++ 0915_트리/subtree.py
@@ -1,45 +1,44 @@
 # dfs 카운트
-def dfs(node):
-    if node == 0:
-        return 0
-    return 1 + dfs(child1[node]) + dfs(child2[node])
+# def dfs(node):
+#     if node == 0:
+#         return 0
+#     return 1 + dfs(child1[node]) + dfs(child2[node])
 
-T = int(input())
-for tc in range(1, T+1):
-    E, N = map(int, input().split())
-    arr = list(map(int, input().split()))
-    child1 = [0]*(E+2)
-    child2 = [0]*(E+2)
+# T = int(input())
+# for tc in range(1, T+1):
+#     E, N = map(int, input().split())
+#     arr = list(map(int, input().split()))
+#     child1 = [0]*(E+2)
+#     child2 = [0]*(E+2)
 
-    for i in range(E):
-        p, c = arr[2*i], arr[2*i+1]
-        if child1[p] == 0:
-            child1[p] = c
-        else:
-            child2[p] = c
+#     for i in range(E):
+#         p, c = arr[2*i], arr[2*i+1]
+#         if child1[p] == 0:
+#             child1[p] = c
+#         else:
+#             child2[p] = c
 
-    ans = dfs(N)
-    print(f"#{tc} {ans}")
+#     ans = dfs(N)
+#     print(f"#{tc} {ans}")
 
 # -------------------------------------
 # 인접리스트 방식
 def dfs(node):
     cnt = 1
-    for nxt in child[node]:
-        cnt += dfs(nxt)
+    for next in child[node]:
+        cnt += dfs(next)
     return cnt
 
 T = int(input())
-for tc in range(1, T+1):
-    E, N = map(int, input().split())
-    arr = list(map(int, input().split()))
+for test_case in range(1,T+1):
+    E,N = map(int,input().split())
+    arr = list(map(int,input().split()))
 
     child = [[] for _ in range(E+2)]
 
     for i in range(E):
-        parent = arr[2*i]
-        c = arr[2*i+1]
-        child[parent].append(c)
-
+        p , c = arr[2*i], arr[2*i+1]
+        child[p].append(c)
+    
     ans = dfs(N)
-    print(f"#{tc} {ans}")
+    print(f'#{test_case} {ans}')
