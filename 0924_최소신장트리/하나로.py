d162b83ab001d12a2ded8283961bc8231a2b78f1
duuujin <enwls5626@gmail.com>
2025-09-24 17:40:46 +0900

    최소신장트리_하나로

---
+++ 0924_최소신장트리/하나로.py
@@ -0,0 +1,28 @@
+import heapq
+
+T = int(input())
+for test_case in range(1,T+1):
+    n = int(input())
+    x_list = list(map(int,input().split()))
+    y_list = list(map(int,input().split()))
+    e = float(input())
+    
+    vertices = [(x, y) for x, y in zip(x_list, y_list)]
+
+    visited = [False] * n
+    min_heap = [(0,0)]
+    total_cost = 0
+    cnt = 0
+
+    while min_heap and cnt < n:
+        cost,u = heapq.heappop(min_heap)
+        if visited[u]: continue
+        visited[u] = True
+        total_cost += cost
+        cnt += 1
+        print(min_heap)
+        for v in range(n):
+            if not visited[v]:
+                dist = (vertices[u][0] - vertices[v][0])**2 + (vertices[u][1] - vertices[v][1])**2
+                heapq.heappush(min_heap, (dist * e, v))
+    print(f'#{test_case} {round(total_cost)}')
