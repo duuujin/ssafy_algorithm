4d368e3e74bc7605e3801826f3a9c4a758069b80
duuujin <enwls5626@gmail.com>
2025-10-01 13:55:27 +0900

    1001_최단경로풀기

---
+++ 0925_최단경로/보급로.py
@@ -0,0 +1,30 @@
+import math, heapq
+dxy = [[0,1],[0,-1],[1,0],[-1,0]]
+
+def bfs(arr):
+    distance = [[math.inf] * n for _ in range(n)]
+    hq =[]
+    distance[0][0] = 0
+    heapq.heappush(hq, (0,0,0))
+
+    while hq:
+        dist, x, y = heapq.heappop(hq)
+        if x == n-1 and y == n-1:
+            return dist
+        for dx, dy in dxy:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < n and 0 <= ny < n :
+                nextdist = dist + arr[nx][ny]
+                if nextdist < distance[nx][ny]:
+                    distance[nx][ny] = nextdist
+                    heapq.heappush(hq, (nextdist,nx,ny))
+
+
+T = int(input())
+for test_case in range(1,T+1):
+    n = int(input())
+    arr = [list(map(int,input())) for _ in range(n)]
+    
+
+    total_cnt = bfs(arr)
+    print(f'#{test_case} {total_cnt}')
