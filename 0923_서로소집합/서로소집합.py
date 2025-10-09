f48b30056e4ebbcfeb2529c38571f48bd166f99c
DESKTOP-BTDJ0S7\Administrator <enwls5626@gmail.com>
2025-10-09 21:49:52 +0900

    수정

--- 0923_서로소집합/서로소집합.py
+++ 0923_서로소집합/서로소집합.py
@@ -1,30 +1,28 @@
-def find_set(x):
+def find(x):
     if x != p_list[x]:
-        p_list[x] = find_set(p_list[x])
+        p_list[x] = find(p_list[x])
     return p_list[x]
 
-def union_set(x,y):
-    px = find_set(x)
-    py = find_set(y)
-
-    if px != py:
+def union(x,y):
+    px = find(x)
+    py = find(y)
+    if px < py:
         p_list[py] = px
-
+    else:
+        p_list[px] = py
 
 T = int(input())
 for test_case in range(1,T+1):
-    n,m = map(int,input().split())
+    n, m = map(int,input().split())
+    ans = []
     p_list = list(range(n+1))
-    rank = [0] * (n+1)
-    answer = []
     for _ in range(m):
         e, a, b = map(int,input().split())
-        if e == 0 :
-            union_set(a,b)
+        if e == 0:
+            union(a,b)
         else:
-            if find_set(a) == find_set(b):
-                answer.append("1")
+            if find(a) == find(b):
+                ans.append('1')
             else:
-                answer.append("0")
-    
-    print(f'#{test_case} {"".join(answer)}')
+                ans.append('0')
+    print(f'#{test_case} {"".join(ans)}')
